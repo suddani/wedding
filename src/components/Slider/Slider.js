e938f6654bd0f66aca2797a023c9e6f8fb660f4a
Suddani <suddani@googlemail.com>
2019-09-01 13:49:58 +0200

    Faster loading

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,13 @@
 import React, {useRef} from 'react';
 import useOnScroll from './../../hooks/useOnScroll'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import './Slider.css'
 import banner from './../../assets/images/main.jpeg';
+import banner_small from './../../assets/images/main_small.jpeg';
 
 export default function Slider(props) {
+  const biggerThan700 = useMediaQuery('(min-width:700px)');
   // const [top, setTop] = useState(0);
   const container = useRef();
   useOnScroll(() => {
@@ -17,7 +20,7 @@ export default function Slider(props) {
     <section className='slider' style={{height: (props.height||625)+'px'}}>
       <ul>
         <li>
-          <div ref={container} style={{backgroundImage: `url(${banner})`}}></div>
+          <div ref={container} style={{backgroundImage: `url(${biggerThan700 ? banner : banner_small})`}}></div>
         </li>
       </ul>
       <div>{props.text}</div>
