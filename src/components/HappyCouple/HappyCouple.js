54b18e0f6fed062b85105b64fd7be903ec50774b
Suddani <suddani@googlemail.com>
2019-09-01 04:27:46 +0200

    Couple

--- src/components/HappyCouple/HappyCouple.js
+++ src/components/HappyCouple/HappyCouple.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 import './HappyCouple.css';
 
+import bride from './../../assets/images/bride.jpg';
+import groom from './../../assets/images/groom.jpg';
+
 export default function HappyCouple(props) {
   return (
     <section className="HappyCouple">
       <h1>The happy couple</h1>
+      <p className="introduction">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan, efficitur dolor fermentum, tincidunt metus.
+      </p>
+      <div className="bride">
+        <img src={bride}></img>
+        <div></div>
+      </div>
+      <p className="brideText"><b>Mariia Sendziuk</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan, efficitur dolor fermentum, tincidunt metus.</p>
+      <div className="groom">
+        <img src={groom}></img>
+        <div></div>
+      </div>
+      <p className="groomText"><b>Daniel Sudmann</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultrices malesuada ante quis pharetra. Nullam non bibendum dolor. Ut vel turpis accumsan, efficitur dolor fermentum, tincidunt metus.</p>
     </section>
   );
 }
