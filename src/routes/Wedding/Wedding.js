005e2adef7b1630c20a0285e6c26ca8a12427508
Suddani <suddani@googlemail.com>
2020-02-18 02:27:39 +0100

    Does not work.

--- src/routes/Wedding/Wedding.js
+++ src/routes/Wedding/Wedding.js
@@ -5,17 +5,21 @@ import './Wedding.css';
 
 import restaurant from './restaurant.jpeg';
 
+const church = "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL2VtYmVkL3YxL3BsYWNlP3E9cGxhY2VfaWQ6RWxKTmVXdG9ZV2xzWVNCSVlXbDJiM0p2Ym5OcmIyaHZJRk4wY21WbGRDd2dNaXdnV21Gc2FYTmphSGxyZVN3Z1ZHVnlibTl3YVd3bmN5ZHJZU0J2WW14aGMzUXNJRlZyY21GcGJtVXNJRFE0TmpBMElqQVNMZ29VQ2hJSlVYLUJyRVpiTVVjUnp2WG9vS3hSNlo0UUFpb1VDaElKbFR1aE5VUmJNVWNSMWFWUTM5bm1faE0ma2V5PUFJemFTeUNVUnJoT0pSU1JzYTVEU1FkczdubjlOQXY0NWQ5dzFfMA=="
+const party = "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL2VtYmVkL3YxL3BsYWNlP3E9cGxhY2VfaWQ6Q2hJSkkybFZHOHRjTVVjUmh1UXpQWHVIQllZJmtleT1BSXphU3lDVVJyaE9KUlNSc2E1RFNRZHM3bm45TkF2NDVkOXcxXzA="
+
+const toPartMake = (data) => atob(data)
 function Wedding({t}) {
   return <section className="Wedding">
     <div className="banner" style={{backgroundImage: `url(${restaurant})`}}></div>
     <div className="text">
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </div>
-    <iframe width="1280" height="450" frameborder="0" src={"https://www.google.com/maps/embed/v1/place?q=place_id:ElJNeWtoYWlsYSBIYWl2b3JvbnNrb2hvIFN0cmVldCwgMiwgWmFsaXNjaHlreSwgVGVybm9waWwncydrYSBvYmxhc3QsIFVrcmFpbmUsIDQ4NjA0IjASLgoUChIJUX-BrEZbMUcRzvXooKxR6Z4QAioUChIJlTuhNURbMUcR1aVQ39nm_hM&key="+process.env.REACT_APP_MAPS_API_PUBLIC} allowfullscreen></iframe>
+    <iframe width="1280" height="450" frameborder="0" src={toPartMake(church)} allowfullscreen></iframe>
     <div className="text">
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </div>
-    <iframe width="1280" height="450" frameborder="0" src={"https://www.google.com/maps/embed/v1/place?q=place_id:ChIJI2lVG8tcMUcRhuQzPXuHBYY&key="+process.env.REACT_APP_MAPS_API_PUBLIC} allowfullscreen></iframe>
+    <iframe width="1280" height="450" frameborder="0" src={toPartMake(party)} allowfullscreen></iframe>
   </section>;
 }
 
