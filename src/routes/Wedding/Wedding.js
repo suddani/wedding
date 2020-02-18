8415eaa3ad6a8c8d262e15cc0465b1bd05cf1a8e
Suddani <suddani@googlemail.com>
2020-02-18 01:51:45 +0100

    Restricted browser key this time ;P

--- src/routes/Wedding/Wedding.js
+++ src/routes/Wedding/Wedding.js
@@ -11,7 +11,7 @@ function Wedding({t}) {
     <div className="text">
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </div>
-    <iframe width="1280" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ElJNeWtoYWlsYSBIYWl2b3JvbnNrb2hvIFN0cmVldCwgMiwgWmFsaXNjaHlreSwgVGVybm9waWwncydrYSBvYmxhc3QsIFVrcmFpbmUsIDQ4NjA0IjASLgoUChIJUX-BrEZbMUcRzvXooKxR6Z4QAioUChIJlTuhNURbMUcR1aVQ39nm_hM&key=AIzaSyBChgGsTw5F1sOi5_M_KeotNVVoE_K_2zM" allowfullscreen></iframe>
+    <iframe width="1280" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ElJNeWtoYWlsYSBIYWl2b3JvbnNrb2hvIFN0cmVldCwgMiwgWmFsaXNjaHlreSwgVGVybm9waWwncydrYSBvYmxhc3QsIFVrcmFpbmUsIDQ4NjA0IjASLgoUChIJUX-BrEZbMUcRzvXooKxR6Z4QAioUChIJlTuhNURbMUcR1aVQ39nm_hM&key=AIzaSyD1JW9Sv4O5-RaHmcvACMwEtMq7tiynO2k" allowfullscreen></iframe>
   </section>;
 }
 
