d866a46c1533cae688f914a4be9434a61d6c8954
Suddani <suddani@googlemail.com>
2020-02-18 02:00:14 +0100

    Second map

--- src/routes/Wedding/Wedding.js
+++ src/routes/Wedding/Wedding.js
@@ -12,6 +12,10 @@ function Wedding({t}) {
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
     </div>
     <iframe width="1280" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ElJNeWtoYWlsYSBIYWl2b3JvbnNrb2hvIFN0cmVldCwgMiwgWmFsaXNjaHlreSwgVGVybm9waWwncydrYSBvYmxhc3QsIFVrcmFpbmUsIDQ4NjA0IjASLgoUChIJUX-BrEZbMUcRzvXooKxR6Z4QAioUChIJlTuhNURbMUcR1aVQ39nm_hM&key=AIzaSyD1JW9Sv4O5-RaHmcvACMwEtMq7tiynO2k" allowfullscreen></iframe>
+    <div className="text">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </div>
+    <iframe width="1280" height="450" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJI2lVG8tcMUcRhuQzPXuHBYY&key=AIzaSyD1JW9Sv4O5-RaHmcvACMwEtMq7tiynO2k" allowfullscreen></iframe>
   </section>;
 }
 
