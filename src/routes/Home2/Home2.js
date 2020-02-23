dc8cd91e1db5c145cc8089193bf24d1eb5edec57
Suddani <suddani@googlemail.com>
2020-02-23 04:00:49 +0100

    I am Root. We are Root.

--- src/routes/Home2/Home2.js
+++ src/routes/Home2/Home2.js
@@ -2,24 +2,12 @@ import React from 'react';
 import { withTranslation, Trans } from 'react-i18next';
 
 import CountDown from './../../components/CountDown';
-import Slider from './../../components/Slider';
-import banner from './../../assets/images/main.jpeg';
-import banner_small from './../../assets/images/main_small.jpeg';
 import main from './main3.png';
 import color from './color.png';
 
 import './Home2.css'
 
 function Home2({t}) {
-  const sliderText = (
-    <div className="saveTheDate">
-      <Trans i18nKey="savethedate" ns="home">
-        <div>Save</div><div>The Date</div>
-      </Trans>
-      <div>-10.05.2020-</div>
-      <div><CountDown></CountDown></div>
-    </div>);
-
   return (
     <section className="Home2">
       <h1 style={{display: 'none'}}>Home</h1>
