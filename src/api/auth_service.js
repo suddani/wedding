4ec05cbff94ad5d87617353379b3c73b9e42d5db
Suddani <suddani@googlemail.com>
2020-03-01 21:49:25 +0100

    ALL SORTS OF THINGS

--- src/api/auth_service.js
+++ src/api/auth_service.js
@@ -1,17 +1,45 @@
-
 const auth_host = process.env.REACT_APP_STAGE === 'dev' ? process.env.REACT_APP_API_ENDPOINT : 'api.daniel-mariia.wedding'
 
 export function requestUser(token, user_id) {
   return fetch(
-    `//${auth_host}/auth/user/${user_id||'-1'}?jwt=${token}`, {
+    `//${auth_host}/auth/user/${user_id||'-1'}`, {
     method: "GET",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
     },
     credentials: "same-origin"
   }).then(
     response => response.json(), e => console.log(e)
   );
+}
+
+export function putUser(token, user_id, email, password) {
+  return fetch(
+    `//${auth_host}/auth/user/${user_id||'-1'}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password
+    }),
+    credentials: "same-origin"
+  }).then(
+    response => requestUser(token, user_id)
+  );
+}
+
+export function updateUser(user_id, email, password, jwt, refresh_token, setAccessToken) {
+  return putUser(jwt, user_id, email, password).catch(
+    _=> requestAccessKey(refresh_token).then(
+      access => {
+        setAccessToken(access.access_token);
+        return putUser(access.access_token, user_id, email, password);
+      }
+  ));
 }
 
 export function requestAccessKey(token, type) {
--- src/routes/Account/Account.js
+++ src/routes/Account/Account.js
@@ -1,13 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { withTranslation } from 'react-i18next';
 import { store } from 'react-notifications-component';
+import { Button, Input, Grid, InputLabel, FormControl } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
+import { updateUser } from '../../api/auth_service';
 
 import './Account.scss';
 
-function Account({t}) {
+function Account({t, user, setUser}) {
+  const [email, setEmail] = useState(user.user.email);
+  const [password, setPassword] = useState("");
+  const [access_token, setAccessToken] = useLocalStorage("access_token", null);
+  const [refresh_token, setRefreshToken] = useLocalStorage("refresh_token", null);
+
+  const history = useHistory();
+
+  function onType(setter) {
+    return (event) => {
+      try {
+        setter(event.target.value);
+      } catch(e) {
+        console.log(e)
+      }
+    }
+  }
+
+  console.log(user)
+
+  function onLogout() {
+    setUser(null);
+    setAccessToken(null);
+    setRefreshToken(null);
+    history.push('/');
+    store.addNotification({
+      title: t("Good bye"),
+      message: t('You have been logged out'),
+      type: "danger",
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animated", "fadeIn"],
+      animationOut: ["animated", "fadeOut"],
+      dismiss: {
+        duration: 10000
+      }
+    });
+  }
+
+  function onSubmit() {
+    updateUser(user.user.id, email, password, access_token, refresh_token, setAccessToken).then(
+      userData => {
+        store.addNotification({
+          title: t("Updated"),
+          message: t('Update completed'),
+          type: "default",
+          insert: "top",
+          container: "top-right",
+          animationIn: ["animated", "fadeIn"],
+          animationOut: ["animated", "fadeOut"],
+          dismiss: {
+            duration: 10000
+          }
+        });
+        setUser(userData);
+      }
+    ).catch( _ => history.push('/'));
+  }
+
   return <section>
-    Account
+    <Grid container direction="column" spacing={1} justify='center' alignContent='center'>
+      <Grid item>
+        <FormControl>
+          <InputLabel id="email-label">{t('Email')}</InputLabel>
+          <Input labelid="email-label" name="email" type="email" value={email} onChange={onType(setEmail)}></Input>
+        </FormControl>
+      </Grid>
+      <Grid item>
+        <FormControl>
+          <InputLabel id="password-label">{t('Password')}</InputLabel>
+          <Input labelid="password-label" name="password" type="password" value={password} onChange={onType(setPassword)}></Input>
+        </FormControl>
+      </Grid>
+      <Grid item ><Button variant="contained" onClick={onSubmit} color="secondary">{t('Update')}</Button></Grid>
+      <Grid item ><Button variant="contained" onClick={onLogout} color="danger">{t('Logout')}</Button></Grid>
+    </Grid>
   </section>;
 }
 
