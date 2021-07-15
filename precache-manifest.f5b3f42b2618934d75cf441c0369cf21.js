86c134cfc4d5e280031d379fd0325cb0897881d7
Daniel Sudmann <suddani@gmail.com>
2021-07-15 16:12:15 +0200

    Low On Caffeine, Please Forgive Coding Style

---
+++ precache-manifest.f5b3f42b2618934d75cf441c0369cf21.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc76b009c4d7cb693e5ac0b31c13b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8acdd964387c75ef5bc6",
+    "url": "/static/css/2.609d5137.chunk.css"
+  },
+  {
+    "revision": "edb65dca5e2ac24ae5bb",
+    "url": "/static/css/main.a2a307de.chunk.css"
+  },
+  {
+    "revision": "8acdd964387c75ef5bc6",
+    "url": "/static/js/2.865ac35b.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/static/js/2.865ac35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb65dca5e2ac24ae5bb",
+    "url": "/static/js/main.7106610e.chunk.js"
+  },
+  {
+    "revision": "f8373ff098d46af8f83d",
+    "url": "/static/js/runtime-main.244f0e60.js"
+  },
+  {
+    "revision": "fd629f5aed741170d3ce554cac09f9e9",
+    "url": "/static/media/bigEvent.fd629f5a.jpg"
+  },
+  {
+    "revision": "d8cb62bd92d9d8855bd468b5f7e2a57c",
+    "url": "/static/media/church.d8cb62bd.jpg"
+  },
+  {
+    "revision": "9a19f0cd2bcba679b10e52f420c98a54",
+    "url": "/static/media/firstOktoberfest.9a19f0cd.jpg"
+  },
+  {
+    "revision": "14f826e1520015f3cf9e0b9506ed5f5a",
+    "url": "/static/media/firstSelfie.14f826e1.jpg"
+  },
+  {
+    "revision": "2eb81fa13ced29efd1960e60bed6badf",
+    "url": "/static/media/germanyYes.2eb81fa1.jpg"
+  },
+  {
+    "revision": "1abc10fd8df95d130ce173e45df658da",
+    "url": "/static/media/greatProject.1abc10fd.jpg"
+  },
+  {
+    "revision": "3648117fbd1162b03d4b224e52ce5256",
+    "url": "/static/media/heart.3648117f.svg"
+  },
+  {
+    "revision": "68e3fa7e5a53bbf00cbbec3d7202b027",
+    "url": "/static/media/home.68e3fa7e.png"
+  },
+  {
+    "revision": "2e77a888d155391df9f05ca042e09b40",
+    "url": "/static/media/itsLove.2e77a888.jpg"
+  },
+  {
+    "revision": "8fd75dc7c41b90b14d3019b8612aa43b",
+    "url": "/static/media/justFriends.8fd75dc7.jpg"
+  },
+  {
+    "revision": "6d12dbe47d39a680d1f4eeae13f79026",
+    "url": "/static/media/restaurant.6d12dbe4.jpeg"
+  },
+  {
+    "revision": "bd1c2612e82ec7dfe344c19d4f9b07e9",
+    "url": "/static/media/sheYes.bd1c2612.jpg"
+  },
+  {
+    "revision": "99e8b8803424faa236a27e1d351dbd94",
+    "url": "/static/media/tips.99e8b880.jpg"
+  },
+  {
+    "revision": "4fba39be3097378946a3c55455d3fe43",
+    "url": "/static/media/top_left_v10.4fba39be.png"
+  },
+  {
+    "revision": "7d648000d06aa2b9e46f6e5afbf0884b",
+    "url": "/static/media/top_right_v10.7d648000.png"
+  },
+  {
+    "revision": "a33983f7174897c088b9731dc84919d5",
+    "url": "/static/media/under_monogram.a33983f7.png"
+  }
+]);
